0a302821883d472459b1bad5fb29d8d15d286168
Comentador <47295402+Comentador@users.noreply.github.com>
2019-07-06 13:31:02 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@
 
     $opc["chat_id"]=$mensagem["chat"]["id"];
     $opc["texto"]=$mensagem["text"];
-    $opc["message_id"]=$mensagem["message_id"]+1;
+    $opc["message_id"]=$mensagem["message_id"]-2;
     $_SESSION['message']=$opc["message_id"];
     $motor = new Divulga();
     $strings = new Strings();
