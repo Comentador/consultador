3530f9daf2a46a28f395fe667927af8f23b5627b
Comentador <47295402+Comentador@users.noreply.github.com>
2019-08-07 13:24:32 -0700

    Update BotC.php

--- BotC.php
+++ BotC.php
@@ -4,9 +4,18 @@ class Divulga{
 
 	public $str;
 	public $id;
+	public $db;
 
 	public function __construct(){
 		$this->str = new Strings();
+		/*try{
+
+			$this->db = new PDO("mysql:dbname=mozdev;host=35.178.176.93", "root", "dados123");
+			
+
+		}catch(PDOexception $e){
+			return $e->getMessage();
+		}*/
 	}
 
 	protected function apiRequest($metodo, $parametro){
@@ -45,7 +54,7 @@ class Divulga{
 		$parametro = array(
 			"chat_id"=>$opc["chat_id"],
 			"text"=>$msg,
-			"message_id"=>$_SESSION["message"],
+			"message_id"=>$opc["message_id"],
 		);
 
 		$this->apiRequest("editMessageText", $parametro);
@@ -78,7 +87,6 @@ class Divulga{
 		else if($cb_data == "pode"){
 			$text = "Seja bem vindo";
 			$this->answerCallbackQuery($cb_id, false, 3, $text);
-			$this->sendMessage($opc, $_SESSION['message']);
 		}
 	}
 
@@ -95,6 +103,52 @@ class Divulga{
 		$this->apiRequest("sendMessage", $parametro);
 	}
 
+	public function saveID($id){
+
+		$abertura = fopen("id.txt", "w+");
+		$conteudo = $id;
+		$escrita = fwrite($abertura, $conteudo);
+		fclose($abertura);
+	}
+
+	public function registrar($user, $lang, $passwd){
+		
+			$sql = $this->db->prepare("SELECT * FROM usuario WHERE user =".$user);
+			$sql->execute();
+			$much = array();
+			if($sql->rowCount() > 0){
+				
+				return "O usuário ja esta cadastrado";
+			
+			}else{
+
+				$sql = $this->db->prepare("INSERT INTO usuario SET user=:usr, senha=:passw, linguagem=:lang");
+				$sql->bindValue(":usr", $user);
+				$sql->bindValue(":passw", $lang);
+				$sql->bindValue(":lang", $passwd);
+				$sql->execute();
+
+				return "Obrigado por se registrar. A mozDevs agradece...";
+			}
+
+	}
+
+	public function lista ($linguagem, $tipo=null){
+
+		$sql = $this->db->prepare("SELECT * FROM usuario WHERE linguagem=:lang");
+		$sql->bindValue(":lang", $linguagem);
+		$sql->execute();
+
+		$list = array();
+
+		if($sql->rowCount() > 0){
+			$list = $sql->fetchAll();
+		}
+
+		return $list;
+	}
+
+
 }
 
 
@@ -102,9 +156,27 @@ class Strings{
 
 	public $fala = array(
 		"primeira"=>"opa fion",
-		"botoes"=>array(
+		"menu"=>array(
 			"inline_keyboard"=>array(
-				array(array("text"=>"testando", "callback_data"=>"avisei"), array("text"=>"ainda", "callback_data"=>"pode")),
+				array(array("text"=>"Registro", "callback_data"=>"reg")),
+				array(array("text"=>"Procurar", "callback_data"=>"search"), array("text"=>"Parceria", "callback_data"=>"parser")),
+				array(array("text"=>"Login", "callback_data"=>"log"), array("text"=>"Ferramentas", "callback_data"=>"tools")),
+				array(array("text"=>"Mais...", "callback_data"=>"me"))
+			),
+		),
+
+		"Registro"=>array(
+			"inline_keyboard"=>array(
+				array(array("text"=>"Programador", "callback_data"=>"coder"), array("text"=>"Cyber Sec", "callback_data"=>"sec")),
+				array(array("text"=>"Menu", "callback_data"=>"menu"))
+			)
+		),
+
+		"Ferramentas"=>array(
+			"inline_keyboard"=>array(
+				array(array("text"=>"Proxy", "callback_data"=>"proxys")),
+				array(array("text"=>"Projectos", "callback_data"=>"project"), array("text"=>"wSociety", "callback_data"=>"wsociety")),
+
 			)
 		),
 	);
